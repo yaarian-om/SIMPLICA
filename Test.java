b1f94da0db186990f3f2fee02c03a9154cc30dfb
Yaarian <dip.kumar020@gmail.com>
2022-07-15 22:58:46 +0600

    Updating SIMPLICA

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String args[]) {
+        int i = 1234567890;
+        boolean b = true;
+        char c = 'a';
+        short s = 12345;
+        float f = 10.2f;
+        double d = 344.659;
+        System.out.printf("boolean b = %b\n",b);
+        System.out.printf("charater c = %c\n",c);
+        System.out.printf("short s = %d\n",s);
+        System.out.printf("integer i = %d\n",i);
+        System.out.printf("float f = %1f\n",f);
+        System.out.printf("double d = %3f\n",d);
+    }
+}
