4e951d54286e6b7f332e2dfee2e82a40b2cf6a11
Yaarian <dip.kumar020@gmail.com>
2022-07-16 22:43:39 +0600

    Updating SIMPLICA

--- Test.java
+++ Test.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Test {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        int result = 0;
-        System.out.println("1+2+3+4+5+.......+n");
-        System.out.print("Enter the last number of the linear series : ");
-        int n = input.nextInt();
-        for (int i = 1; i <= n; i++) {
-            result = result + i;
-        }
-        System.out.println("The Addtion result of the whole series is = " + result);
+        System.out.print("Enter 1st nmber >> ");
+        int x = input.nextInt();
+        System.out.print("Enter 2nd nmber >> ");
+        int y = input.nextInt();
+        do {
+            System.out.println("Condition Is already wrong");
+            x--;
+        } while (x / y == 0);
     }
 }
