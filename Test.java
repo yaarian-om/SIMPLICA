16558e3ca34ef16392726227e8bddac935e809a0
Yaarian <dip.kumar020@gmail.com>
2022-07-16 19:07:11 +0600

    Updating SIMPLICA

--- Test.java
+++ Test.java
@@ -3,67 +3,13 @@ import java.util.Scanner;
 public class Test {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        int x, numberofCokes = 0, numberofChips = 0;
-        char y = 'a';
-        double price = 0;
-        System.out.println("------------------------------ COKES ----------------------------------");
-        System.out.println("                        Press 1 for COCACOLA                           |");
-        System.out.println("                        Press 2 for PEPSI                              |");
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.print("                        Enter your Choice >> ");
-        x = input.nextInt();
-        // If input type is numaric
-        switch (x) {
-            case 1: {
-                System.out.println("COCACOLA SELECTED");
-                System.out.println("Price is = 25/-");
-                numberofCokes++;
-                price = price + 25 * numberofCokes;
-
-                break;
-            }
-            case 2: {
-                System.out.println("PEPSI SELECTED");
-                System.out.println("Price is = 15/-");
-                numberofCokes++;
-                price = price + 15 * numberofCokes;
-                break;
-            }
-            default: {
-                System.out.println("\nPLEASE ENTER A VALID INPUT");
-                break;
-            }
+        int result = 0;
+        System.out.println("1+2+3+4+5+.......+n");
+        System.out.print("Enter the last number of the linear series : ");
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            result = result + i;
         }
-
-        System.out.println("------------------------------ CHIPS ----------------------------------");
-        System.out.println("                        Press a for KURKURE                            |");
-        System.out.println("                        Press b for LAYS                               |");
-        System.out.println("-----------------------------------------------------------------------");
-        System.out.print("                        Enter your Choice >> ");
-        y = input.next().charAt(0);
-        // If input type is character
-        switch (y) {
-            case 'a': {
-                System.out.println("KURKURE SELECTED");
-                System.out.println("Price is = 25/-");
-                numberofChips++;
-                price = price + 25 * numberofChips;
-                break;
-            }
-            case 'b': {
-                System.out.println("KURKURE SELECTED");
-                System.out.println("Price is = 20/-");
-                numberofChips++;
-                price = price + 20 * numberofChips;
-                break;
-            }
-            default: {
-                System.out.println("\nPLEASE ENTER A VALID INPUT");
-                break;
-            }
-        }
-
-        System.out.println("Your Bill is = " + price + " /- TK");
-
+        System.out.println("The Addtion result of the whole series is = " + result);
     }
 }
