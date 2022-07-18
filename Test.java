56f90539dab4f1e3c93105e3a01bc54bcdb1277e
yaarian-om <dip.kumar020@gmail.com>
2022-07-18 21:29:21 +0600

    UPDATE JAVA SIMPLICA FROM LAPTOP

--- Test.java
+++ Test.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
-
 public class Test {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter 1st nmber >> ");
-        int x = input.nextInt();
-        System.out.print("Enter 2nd nmber >> ");
-        int y = input.nextInt();
-        do {
-            System.out.println("Condition Is already wrong");
-            x--;
-        } while (x / y == 0);
+
+        System.out.print("Enter the row Number : ");
+        int m = input.nextInt();
+        System.out.print("Enter the row Number : ");
+        int n = input.nextInt();
     }
 }
