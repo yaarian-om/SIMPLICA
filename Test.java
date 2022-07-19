ab50c40f46bb665be7e72da1bd77b339311ecbc1
yaarian-om <dip.kumar020@gmail.com>
2022-07-19 13:11:41 +0600

    UPDATING JAVA SIMPLICA

--- Test.java
+++ Test.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Test {
-    public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the row Number : ");
-        int m = input.nextInt();
-        System.out.print("Enter the row Number : ");
-        int n = input.nextInt();
-    }
-}
+import javax.print.attribute.standard.NumberUp;
+        public class Test {
+            public static void main(String args[]) {
+                Scanner input = new Scanner(System.in);
+                ArrayList<Integer>number = new ArrayList<>();
+                System.out.println("Size = "+number.size());
+                number.add(10);
+                number.add(20);
+                number.add(30);
+                number.add(40);
+                System.out.println(number); // Horizontal Print
+                //using for each loop
+                for(int x: number){
+                    System.out.println(x);
+                }
+                System.out.println("Size = "+number.size());
+                // Removing Elements
+                number.set(3,50);
+                System.out.println("After Removing, Number = "+number);
+                // REmoving all elements
+                int x = number.get(3);
+                System.out.println("Getting Value is = "+x);
+
+
+
+
+
+                
+            }
+        }
