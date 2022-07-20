bcde9d34bd71ae3b14a1354ffc40fbdbd437c3eb
yaarian-om <dip.kumar020@gmail.com>
2022-07-20 20:54:44 +0600

    UPDATING JAVA SIMPLICA

--- Test.java
+++ Test.java
@@ -1,33 +1,35 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 import javax.print.attribute.standard.NumberUp;
         public class Test {
             public static void main(String args[]) {
-                Scanner input = new Scanner(System.in);
-                ArrayList<Integer>number = new ArrayList<>();
-                System.out.println("Size = "+number.size());
-                number.add(10);
-                number.add(20);
-                number.add(30);
-                number.add(40);
-                System.out.println(number); // Horizontal Print
-                //using for each loop
-                for(int x: number){
-                    System.out.println(x);
+                String message2 = "We_are_Learning";
+                String message = "We_Are_Learning";
+                System.out.println("Length = "+message.length());
+                System.out.println("Equals = "+message.equals(message2));
+                System.out.println("Equals Ingnoring Case = "+message.equalsIgnoreCase(message2));
+                System.out.println("Contains = "+message.contains("Learning"));
+                System.out.println("is Empty = "+message.isEmpty());
+                System.out.println("Concat = "+message.concat("_Java"));
+                System.out.println("To Upper Case = "+message.toUpperCase());
+                System.out.println("To Lower Case = "+message.toLowerCase());
+                System.out.println("Start With = "+message.startsWith("We"));
+                System.out.println("Ends With = "+message.endsWith("Learning"));
+
+                System.out.println("Trim = "+message.trim());
+                System.out.println("Char At = "+message.charAt(5));
+                System.out.println("Index Of = "+message.indexOf("Are"));
+                System.out.println("Last index of = "+message.lastIndexOf("e"));
+                System.out.println("Replace A -> a = "+message.replace('A','a'));
+                String[] words = message.split("_");
+                for (String string : words) {
+                    System.out.println(string);
                 }
-                System.out.println("Size = "+number.size());
-                // Removing Elements
-                number.set(3,50);
-                System.out.println("After Removing, Number = "+number);
-                // REmoving all elements
-                int x = number.get(3);
-                System.out.println("Getting Value is = "+x);
 
 
 
-
-
-                
             }
         }
