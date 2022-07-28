ee8e3ecb9af7a1bd1352fb7f62fbd4d9b6a3e709
yaarian-om <dip.kumar020@gmail.com>
2022-07-29 00:43:37 +0600

    Updation JAVA SIMPLICA

--- Test.java
+++ Test.java
@@ -1,14 +1,20 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class Test {
     public static void main(String args[]) {
-        LocalTime time = LocalTime.now();
-        System.out.println("Time Class = "+time);
+        // By using Random Class
+        Random rand = new Random();
+        int randomNumber = rand. nextInt(10);// from 0 --> 10
+        System.out.println("Random Value 1 = "+randomNumber+50);
+        int randomNumber1 = rand. nextInt(10)+50;
+        System.out.println("Random Value 2 = "+randomNumber1); // from 50 --> (50+10 = 60)
 
-        // Formatting Time (My choice)
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
-        String currentTime = time.format(formatter);
-        System.out.println("Current Date(My choice format) : "+currentTime);
+        // By using Math Class
+        int randomNumber2 = (int) (Math.random()*20); // from 0 --> 20
+        System.out.println("Random Value 2 = "+randomNumber2); 
+        int randomNumber3 = (int) (Math.random()*20)+30; // from 30 --> (30+20 = 50)
+        System.out.println("Random Value 4 = "+randomNumber3); 
+
+
     }
 }
